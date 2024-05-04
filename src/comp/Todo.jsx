f8c0544901e10b51c03a6312215f5a760817c05d
Smarika Ghimire <smarikaghimire35@gmail.com>
2024-05-04 19:53:54 +0545

    first commit

--- src/comp/Todo.jsx
+++ src/comp/Todo.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Todo = ({ todo, onToggle, onDelete, index }) => {
+  const [deleted, setDeleted] = useState(false);
+
   const handleToggle = () => {
     onToggle(todo.id);
+    setDeleted(true); // Set deleted state to true when toggling
+    setTimeout(() => {
+      onDelete(todo.id); // Delete the todo item after 2 seconds
+    }, 2000);
   };
 
-  const handleDelete = () => {
-    onDelete(todo.id);
-  };
+  if (deleted) {
+    return null; // Return null to hide the todo item when deleted
+  }
 
   return (
     <li className={todo.completed ? "completed" : ""}>
